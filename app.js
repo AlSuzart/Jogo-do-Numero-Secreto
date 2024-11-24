1a07e80ae1a68ce64cf7864248da7071d5f20ed4
AlSuzart <140514065+AlSuzart@users.noreply.github.com>
2024-11-24 12:58:36 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,7 +19,16 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+   //para usar outro de voz nativo do crhome e edge(web speech api)
+    if ('speechSynthesis' in window) {
+    let utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR'; 
+    utterance.rate = 1.2; 
+    window.speechSynthesis.speak(utterance); 
+    } else {
+    console.log("Web Speech API não suportada neste navegador.");
+    }
+    //responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 //para usar outro de voz nativo do crhome e edge(web speech api)
 // if ('speechSynthesis' in window) {
